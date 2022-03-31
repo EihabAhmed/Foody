327baca4a7c35cf45fada9466b176e9dd0606a61
EihabAhmed <eihabahmed@gmail.com>
2022-03-31 11:29:11 +0200

    Finished 8.4. RecipesFragment - Connect the Dots

--- app/src/main/java/com/bbk/foody/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/bbk/foody/bindingadapters/RecipesRowBinding.kt
@@ -5,13 +5,22 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import coil.load
 import com.bbk.foody.R
 
 class RecipesRowBinding {
 
     companion object {
 
-        @BindingAdapter("setNumberOfLikes")
+        @BindingAdapter("loadImageFromUrl")
+        @JvmStatic
+        fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
+            imageView.load(imageUrl) {
+                crossfade(600)
+            }
+        }
+
+                @BindingAdapter("setNumberOfLikes")
         @JvmStatic
         fun setNumberOfLikes(textView: TextView, likes: Int) {
             textView.text = likes.toString()
