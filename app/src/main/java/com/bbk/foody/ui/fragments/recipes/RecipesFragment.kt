6eb71fdefd2033d3a7c403101996383c12c5d1f7
Eihab Ahmed <eihabahmed@gmail.com>
2022-04-02 11:31:30 +0200

    Finished 13.3. Navigate to RecipesBottomSheet

--- app/src/main/java/com/bbk/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/bbk/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -9,7 +9,9 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bbk.foody.R
 import com.bbk.foody.adapters.RecipesAdapter
 import com.bbk.foody.databinding.FragmentRecipesBinding
 import com.bbk.foody.util.NetworkResult
@@ -47,6 +49,10 @@ class RecipesFragment : Fragment() {
 
         setupRecyclerView()
         readDatabase()
+
+        binding.recipesFab.setOnClickListener {
+            findNavController().navigate(R.id.action_recipesFragment_to_recipesBottomSheet)
+        }
 
         return binding.root
     }
