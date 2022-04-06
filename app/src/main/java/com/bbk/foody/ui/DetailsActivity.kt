69a3db8d538d1212e39240debc8f61cc94fa7807
Eihab Ahmed <eihabahmed@gmail.com>
2022-04-06 05:58:27 +0200

    Finished 16.1. Design activity_details Layout

---
+++ app/src/main/java/com/bbk/foody/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.bbk.foody.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bbk.foody.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
