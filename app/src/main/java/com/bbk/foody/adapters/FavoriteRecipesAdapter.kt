fece8cab59a95bccc040c8e378d604cb95acd0a0
Eihab Ahmed <eihabahmed@gmail.com>
2022-04-09 20:41:15 +0200

    Finished 22.7. Close Contextual Action Mode

--- app/src/main/java/com/bbk/foody/adapters/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/bbk/foody/adapters/FavoriteRecipesAdapter.kt
@@ -188,4 +188,10 @@ class FavoriteRecipesAdapter(
         ).setAction("Okay") {}
             .show()
     }
+
+    fun clearContextualActionMode() {
+        if (this::mActionMode.isInitialized) {
+            mActionMode.finish()
+        }
+    }
 }
