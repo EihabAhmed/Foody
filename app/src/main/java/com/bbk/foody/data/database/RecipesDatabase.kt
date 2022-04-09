873350fef37ae74e92548f59cf04445eb6523a03
Eihab Ahmed <eihabahmed@gmail.com>
2022-04-09 04:03:55 +0200

    Finished 19.6. Modify RecipesDatabase class

--- app/src/main/java/com/bbk/foody/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/bbk/foody/data/database/RecipesDatabase.kt
@@ -3,11 +3,12 @@ package com.bbk.foody.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.bbk.foody.data.database.entities.FavoritesEntity
 import com.bbk.foody.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
-    version = 2,
+    entities = [RecipesEntity::class, FavoritesEntity::class],
+    version = 1,
     exportSchema = false
 )
 @TypeConverters(RecipesTypeConverter::class)
