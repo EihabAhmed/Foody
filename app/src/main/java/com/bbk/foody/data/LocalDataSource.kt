7909169a4cd8977cb0d32bf5529d6340592560bd
Eihab Ahmed <eihabahmed@gmail.com>
2022-04-10 08:07:16 +0200

    Finished 23.6. Offline Cache - FoodJoke

--- app/src/main/java/com/bbk/foody/data/LocalDataSource.kt
+++ app/src/main/java/com/bbk/foody/data/LocalDataSource.kt
@@ -2,6 +2,7 @@ package com.bbk.foody.data
 
 import com.bbk.foody.data.database.RecipesDao
 import com.bbk.foody.data.database.entities.FavoritesEntity
+import com.bbk.foody.data.database.entities.FoodJokeEntity
 import com.bbk.foody.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -18,12 +19,20 @@ class LocalDataSource @Inject constructor(
         return recipesDao.readFavoriteRecipes()
     }
 
+    fun readFoodJoke(): Flow<List<FoodJokeEntity>> {
+        return recipesDao.readFoodJoke()
+    }
+
     suspend fun insertRecipes(recipesEntity: RecipesEntity) {
         recipesDao.insertRecipes(recipesEntity)
     }
 
     suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) {
         recipesDao.insertFavoriteRecipes(favoritesEntity)
+    }
+
+    suspend fun insertFoodJoke(foodJokeEntity: FoodJokeEntity) {
+        recipesDao.insertFoodJoke(foodJokeEntity)
     }
 
     suspend fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity) {
--- app/src/main/java/com/bbk/foody/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/bbk/foody/viewmodels/MainViewModel.kt
@@ -7,6 +7,7 @@ import android.net.NetworkCapabilities
 import androidx.lifecycle.*
 import com.bbk.foody.data.Repository
 import com.bbk.foody.data.database.entities.FavoritesEntity
+import com.bbk.foody.data.database.entities.FoodJokeEntity
 import com.bbk.foody.data.database.entities.RecipesEntity
 import com.bbk.foody.models.FoodJoke
 import com.bbk.foody.models.FoodRecipe
@@ -27,6 +28,7 @@ class MainViewModel @Inject constructor(
     /** ROOM DATABASE */
     val readRecipes: LiveData<List<RecipesEntity>> = repository.local.readRecipes().asLiveData()
     val readFavoriteRecipes: LiveData<List<FavoritesEntity>> = repository.local.readFavoriteRecipes().asLiveData()
+    val readFoodJoke: LiveData<List<FoodJokeEntity>> = repository.local.readFoodJoke().asLiveData()
 
     private fun insertRecipes(recipesEntity: RecipesEntity) =
         viewModelScope.launch(Dispatchers.IO) {
@@ -36,6 +38,11 @@ class MainViewModel @Inject constructor(
     fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) =
         viewModelScope.launch(Dispatchers.IO) {
             repository.local.insertFavoriteRecipes(favoritesEntity)
+        }
+
+    fun insertFoodJoke(foodJokeEntity: FoodJokeEntity) =
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.local.insertFoodJoke(foodJokeEntity)
         }
 
     fun deleteFavoriteRecipe(favoritesEntity: FavoritesEntity) =
@@ -104,6 +111,11 @@ class MainViewModel @Inject constructor(
             try {
                 val response = repository.remote.getFoodJoke(apiKey)
                 foodJokeResponse.value = handleFoodJokeResponse(response)
+
+                val foodJoke = foodJokeResponse.value!!.data
+                if (foodJoke != null) {
+                    offlineCachFoodJoke(foodJoke)
+                }
             } catch (e: Exception) {
                 foodJokeResponse.value = NetworkResult.Error("Recipes not found.")
             }
@@ -115,6 +127,11 @@ class MainViewModel @Inject constructor(
     private fun offlineCachRecipes(foodRecipe: FoodRecipe) {
         val recipesEntity = RecipesEntity(foodRecipe)
         insertRecipes(recipesEntity)
+    }
+
+    private fun offlineCachFoodJoke(foodJoke: FoodJoke) {
+        val foodJokeEntity = FoodJokeEntity(foodJoke)
+        insertFoodJoke(foodJokeEntity)
     }
 
     private fun handleFoodRecipesResponse(response: Response<FoodRecipe>): NetworkResult<FoodRecipe>? {
